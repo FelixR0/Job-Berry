7ecbdae68dec4ce656e72d122060397d3f36c6b0
Felix <79116375+FelixRodriguezJr@users.noreply.github.com>
2025-10-27 01:22:21 -0400

    Name Change for Button

--- src/components/JobItem.jsx
+++ src/components/JobItem.jsx
@@ -6,7 +6,7 @@ export const JobItem = ({ title, description, src, url }) => {
                 <h5 className="card-title" style={{ color: "#FDCFFA", width: "100%", marginBottom: "5px", display: "-webkit-box", WebkitLineClamp: 2, WebkitBoxOrient: "vertical", overflow: "hidden", textOverflow: "ellipsis" }}>{title}</h5>
                 <p className="card-text" style={{width: "100%", display: "-webkit-box", WebkitLineClamp: 4, WebkitBoxOrient: "vertical", overflow: "hidden", textOverflow: "ellipsis" }}>{description ? description : "No job description found."}</p>
                 <div style={{ position: "absolute", bottom: "10px", left: "50%", transform: "translateX(-50%)", width: "calc(100% - 17px)" }}>
-                    <a href={url} className="btn btn-purple" style={{ display: "block", width: "100%", borderRadius: "15px", fontWeight: "500" }}>Read More</a>
+                    <a href={url} className="btn btn-purple" style={{ display: "block", width: "100%", borderRadius: "15px", fontWeight: "500" }}>Apply Now</a>
                 </div>
             </div>
         </div>
